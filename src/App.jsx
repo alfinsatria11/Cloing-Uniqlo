84f05e93f0e8dcdba2b2d19bfb0e5c3a0f0a9bde
alfinsatria11 <137568135+alfinsatria11@users.noreply.github.com>
2024-02-17 03:51:14 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Register from "./pages/Register";
+import Kategori from "./pages/Kategori";
+import Home from "./pages/Home";
+import Detail from "./pages/Detail";
+import Cart from "./pages/Cart";
+import Wishlist from "./pages/Wishlist";
+import Login from "./pages/Login";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NotFound from "./pages/Notfound";
+import RouterUtil from "./utils/router.util";
+function App() {
+  const [count, setCount] = useState(0);
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/Header" element={<Header />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/Register" element={<Register />} />
+          <Route path="/Detail/:id" element={<Detail />} />
+          <Route path="/Cart" element={<Cart />} />
+          <Route path="/Wishlist" element={<Wishlist />} />
+          <Route path={RouterUtil.KATEGORI} element={<Kategori />} />
+          <Route path="/Footer" element={<Footer />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
